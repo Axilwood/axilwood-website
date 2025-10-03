727ec52eac7c57c0a75b61e976ad83251ba7259e
Arjun Sudharsan <axilwoodpvtltd@gmail.com>
2025-10-04 01:32:35 +0530

    more transitions

--- scripts.js
+++ scripts.js
@@ -1,10 +1,16 @@
+// scripts.js
 document.addEventListener("DOMContentLoaded", function() {
+  // Add fade-in effect to the body
+  document.body.classList.add('loaded');
+
+  // Fetch and inject header
   fetch('/header.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('header-placeholder').innerHTML = data;
     });
 
+  // Fetch and inject footer
   fetch('/footer.html')
     .then(response => response.text())
     .then(data => {
